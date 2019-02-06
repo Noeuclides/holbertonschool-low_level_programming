841f23521af7a3340760b8291f62695847956c94
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-06 23:39:27 +0000

    using puts for print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *puts: print a string
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int string = puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
