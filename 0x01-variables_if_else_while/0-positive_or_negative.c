775da274206ac01352d2557fcc89204ba3bb7cbb
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-07 23:24:51 +0000

    generate random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 /**
  *main - Entry point
  *
- *
  *srand: random values
+ *
  *printf: print n positive, negative or zero
  *
  *Return: Always 0 (success)
@@ -27,12 +27,12 @@ int main(void)
 
 	else if (n < 0)
 	{
-		printf("%d is negative \n", n);
+		printf("%d is negative\n", n);
 	}
 
 	else if (n == 0)
 	{
-		printf("%d is zero \n", n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
