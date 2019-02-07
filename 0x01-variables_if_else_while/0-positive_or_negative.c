054fe6d7c105cff39715b78f4bd3b0602c89ab1f
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-07 23:17:53 +0000

    generate random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *main - Entry point
+ *
+ *
+ *srand: random values
+ *printf: print n positive, negative or zero
+ *
+ *Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int n;
@@ -11,14 +19,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0) {
+
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0) {
+
+	else if (n < 0)
+	{
 		printf("%d is negative \n", n);
 	}
-	else if (n == 0) {
+
+	else if (n == 0)
+	{
 		printf("%d is zero \n", n);
 	}
+
 	return (0);
 }
