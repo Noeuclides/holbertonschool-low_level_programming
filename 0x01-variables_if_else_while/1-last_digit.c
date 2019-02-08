4e390e2ec3f5dfe508dbac288ebb126f9f3f1f1e
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-08 00:40:23 +0000

    takes the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	printf("Last digit of %d is ", n);
+
 	if (n > 5)
 	{
 		printf("%d and is greater than 5\n", (n % 10));
 	}
+
 	else if (n == 0)
 	{
 		printf("%d and is 0\n", (n % 10));
 	}
+
 	else if ((n < 6) && (n != 0))
 	{
 		printf("%d and is less than 6 and not 0\n", (n % 10));
