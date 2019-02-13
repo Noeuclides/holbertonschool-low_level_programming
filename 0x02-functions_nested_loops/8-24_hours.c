60987fe1b1bedefc3b8232ebfd8ea9d6c6ae392c
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-13 19:28:44 +0000

    prinit jack bauer 24 hours long way

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,6 @@
 /**
  *jack_bauer - time format
  *
- *@n: variable
- *
  *Return: m (success)
  */
 
@@ -15,15 +13,14 @@ void jack_bauer(void)
 	int n3;
 	int n4;
 
-
-	for(n1 = 0; n1 < 2; n1++)
+	for (n1 = 0; n1 < 2; n1++)
 	{
-		for(n2 = 0; n2 <= 9; n2++)
+		for (n2 = 0; n2 <= 9; n2++)
 		{
 
-			for(n3 = 0; n3 < 6; n3++)
+			for (n3 = 0; n3 < 6; n3++)
 			{
-				for(n4 = 0; n4 <= 9; n4++)
+				for (n4 = 0; n4 <= 9; n4++)
 				{
 					_putchar(n1 + '0');
 					_putchar(n2 + '0');
@@ -36,21 +33,19 @@ void jack_bauer(void)
 		}
 	}
 	n1 = 2;
-	for(n2 = 0; n2 <= 3; n2++)
+	for (n2 = 0; n2 <= 3; n2++)
+	{
+		for (n3 = 0; n3 < 6; n3++)
 		{
-
-			for(n3 = 0; n3 < 6; n3++)
+			for (n4 = 0; n4 <= 9; n4++)
 			{
-				for(n4 = 0; n4 <= 9; n4++)
-				{
-					_putchar(n1 + '0');
-					_putchar(n2 + '0');
-					_putchar(':');
-					_putchar(n3 + '0');
-					_putchar(n4 + '0');
-					_putchar('\n');
-				}
+				_putchar(n1 + '0');
+				_putchar(n2 + '0');
+				_putchar(':');
+				_putchar(n3 + '0');
+				_putchar(n4 + '0');
+				_putchar('\n');
 			}
 		}
-
+	}
 }
