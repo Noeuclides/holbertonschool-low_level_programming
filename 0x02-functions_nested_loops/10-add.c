f5dc2328179cc58ac9c6b185474e0c2bce518d7d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-13 20:50:59 +0000

    function that returns the sum of two variables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *add - add two numbers
+ *
+ *@a: first variable
+ *
+ *@b: second variable
+ *
+ *Return: result (success)
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
