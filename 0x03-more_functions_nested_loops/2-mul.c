4a1a5833bb63c7765e74b1b98290df05c930f16e
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-14 19:45:22 +0000

    multiplies two integer variables

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *mul - multiplies two variables
+ *
+ *@a: first int variable
+ *
+ *@b: second variable
+ *
+ *_putchar: put character
+ *
+ *Return: 0 always (succeed)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
