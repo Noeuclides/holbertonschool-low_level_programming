cb0c155252954597ed3657f56ea0a6bcaa8b0222
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-15 00:33:06 +0000

    print a character _

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *print_line - print "_"
+ *
+ *@n: times to print "_"
+ *
+ *_putchar: put character
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -19,11 +19,13 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
+/*0x03*/
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif
