28578a9b53e188b6c621533623c66436f92af61f
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-15 02:59:22 +0000

    Fizz Buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - entry point
+ *
+ *printf: print number or Fuzz or Buzz or FizzBuzz
+ *
+ *Return: 0 always (succeed)
+ */
+
+int main(void)
+{
+	int n;
+	char *m;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n%5 == 0)
+		{
+			m = "FizzBuzz";
+			printf("%s ", m);
+		}
+		else if (n % 5 == 0)
+		{
+			m = "Buzz";
+			printf("%s ", m);
+		}
+		else if (n % 3 == 0)
+		{
+			m = "Fizz";
+			printf("%s ", m);
+		}
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
+}
