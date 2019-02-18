9027f48a146eae2b60b508e860288141eabe9a06
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-18 05:26:58 +0000

    fibonacci sum of even valued terms, not exceeding 4000000, initialize variable n = 0

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 	long y = 0;
 	long z;
 	long i;
-	long n;
+	long n = 0;
 
 	for (i = 0; i < 50; i++)
 	{
 		z = x + y;
 
-		if (z % 2 == 0 && z < 4000000)
+		if (z % 2 == 0 && n < 4000000)
 		{
 			n += z;
 		}
