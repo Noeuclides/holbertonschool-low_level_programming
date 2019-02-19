94ecceca6456c78e93ae0b47f65d16018bd19ea3
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-19 21:41:29 +0000

    print a sentence backwards

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+*print_rev - print string
+*
+*@str: a character variable
+*
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int len;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}
