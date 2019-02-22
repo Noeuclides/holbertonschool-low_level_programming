a96d54ab400b032fb234eb2aa571b3de144ae297
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-22 19:35:20 +0000

    compare v3?

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 	int comp;
 
-	if (s1[i] - s2[i] != 0)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		comp = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+		{
+			comp = s1[i] - s2[i];
+			s1[i + 1] = '\0';
+		}
+		else
+			comp = 0;
 	}
-	else
-		comp = 0;
 
 	return (comp);
 }
