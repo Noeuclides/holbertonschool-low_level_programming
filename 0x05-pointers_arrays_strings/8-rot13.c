f6603484b3a87eac2960aeec793647fd4f38ce63
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-24 19:22:27 +0000

    rot13 function first attemp

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ **rot13 - change all lowercase to upper
+ *
+ *@s: pointer array
+ *
+ *Return: return character succeed
+ *
+ */
+
+char *rot13(char *s)
+{
+	int i;
+char UPPER[] = "ABCDEFGHIJKLM
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
+			s[i] += 13;
+	}
+
+	return (s);
+}
