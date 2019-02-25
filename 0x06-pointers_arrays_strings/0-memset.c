f28a23fdace7b3edb8cc9942835afb2f877268ca
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-25 16:15:46 +0000

    memset function replacing the first n chars of an array

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,26 @@
 #include "holberton.h"
 
 /**
- **_memset -
+ **_memset - replace the first characters of an array by a set char n times
  *
+ *@s: character array
  *
+ *@b: character which its going to replace the first chars of the main array
+ *
+ *@n: number of times in which our char variable its going to be put
  *
  *Return: char when succeed
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
 
-
-
-
-
-
-
-
+	return (s);
 
 }
