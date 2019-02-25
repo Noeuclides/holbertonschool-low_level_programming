0bd0d522e30bf796f95cd19c97069fea64f1b693
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-25 00:47:59 +0000

    rot13 function v2

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -12,12 +12,14 @@
 char *rot13(char *s)
 {
 	int i;
-char UPPER[] = "ABCDEFGHIJKLM
+	/*char UPPER[] = "ABCDEFGHIJKLM";*/
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
 			s[i] += 13;
+		else if ((s[i] > 77 && s[i] < 91) || (s[i] > 110 && s[i] < 123))
+			s[i] -= 13;
 	}
 
 	return (s);
