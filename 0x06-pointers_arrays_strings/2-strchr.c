a74c3a05e5f0a18758ec6f8736183f7bcd0948bd
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-25 19:25:33 +0000

    updating without = v2

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -13,15 +13,28 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
+	unsigned int n = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			s[i] = s[i];
+			n = i;
 		}
+		if (n != 0)
+			s[i + 1] = '\0';
 	}
 
+	if (n == 0)
+	{
+		return ('00');
+	}
+	else
+	{
+		for (i = n; s[i] != '\0'; i++)
+		{
+		}
+	}
 	return (s);
 
 }
