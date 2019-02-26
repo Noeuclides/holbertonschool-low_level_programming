8ffe02042a7e530ee5ff213df99b0e6ccea148ad
Noeuclides <euclidesnoeuclides@gmail.com>
2019-02-26 12:29:31 +0000

    multiply diagonals and match characters function

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *_strpbrk - examine a string and found the first character
+ *
+ *@s: string to be examinated
+ *
+ *@accept: character to be found
+ *
+ *Return: char when succeed
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (*(s + i));
+			}
+		}
+	}
+	return (0);
+}
