1c704528326d3dd9a35736971f4ccbc16b2f0bb0
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-02 22:39:47 +0000

    print argv[0]: name of file

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 /**
+*main - entry point
 *
+*@argc: count of strings
 *
+*@argv: array of pointers
 *
-*
-*
+*Return: 0 when succeed
 **/
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	argc = 0;
+
 	if (argc == 0)
 	{
 		printf("%s\n", argv[0]);
 	}
 
-	return (0);
+	return (argc);
 
 }
