08b2ef5fa4932280baece90089010412df3ebfd1
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-07 21:00:05 +0000

    2d array init 0

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,27 +16,34 @@ int **alloc_grid(int width, int height)
 	int **matrix;
 	int i, j;
 
-	matrix = malloc(sizeof(int *) * (width));
-
-	if (!matrix)
-		return (NULL);
-
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
+	matrix = malloc(sizeof(int *) * (width));
+
+	if (!matrix)
+	{
+		free(matrix);
+		return (NULL);
+	}
+
 	for (i = 0; i < width; i++)
 	{
-		matrix[i] = malloc(sizeof(int) * height);
-
-		for (j = 0; j < height; j++)
+		matrix[i] = malloc(sizeof(int) * (height));
+		if (!matrix[i])
 		{
-			if (!matrix[i])
+			for (; i >= 0; i--)
 			{
-				free(matrix);
+				free(matrix[i]);
 				return (NULL);
 			}
+			free(matrix);
+		}
+		for (j = 0; j < height; j++)
+		{
 			matrix[i][j] = 0;
 		}
 	}
+
 	return (matrix);
 }
