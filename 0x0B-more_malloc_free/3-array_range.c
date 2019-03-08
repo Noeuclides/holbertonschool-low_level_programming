63ff648b741c965775264f4da28e7f8ddf7d3c30
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-08 01:52:22 +0000

    return array with a range without i <= num

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -24,7 +24,7 @@ int *array_range(int min, int max)
 
 		if (!p)
 			return (NULL);
-		for (i = 0, j = min; i <= num + 1 && j <= max; i++, j++)
+		for (i = 0, j = min; j <= max; i++, j++)
 		{
 			p[i] = j;
 		}
