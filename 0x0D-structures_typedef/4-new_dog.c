d1785a8d2b6e830f0d9553760bdcbf82e60cbed9
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-12 21:07:10 +0000

    create a copy of my dog free(name),(owner)(all)

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "dog.h"
@@ -60,13 +60,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	snoopy->name = _strdup(name);
 	if (snoopy->name == NULL)
-		free(snoopy->name);
+		free(name);
+
 	snoopy->age = age;
 	snoopy->owner = _strdup(owner);
 	if (snoopy->owner == NULL)
 	{
 		if (snoopy->name == NULL)
-			free(snoopy->name);
+			free(name);
+		free(owner);
 		free(snoopy);
 	}
 
