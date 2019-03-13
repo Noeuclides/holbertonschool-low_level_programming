0e9610133df20d21264d46347605b53b173bd53c
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-13 22:07:34 +0000

    function that have an pointer function that compares two integers v2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -9,20 +9,24 @@
  *
  *@cmp: function that compares an int with 0
  *
- * Return: 0 if is false, i when success
+ * Return: -1 if does not match any element, i when success
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array && cmp)
 	{
+		if (size <= 0)
+			return (-1);
+
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
 				return (i);
 		}
-	}
 
-	return (0);
+	}
+	return (-1);
 }
