5aaf1da0f8484f2590834b834fe41fc51548aa29
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-13 20:15:43 +0000

    function that prints name

---
+++ 0x0E-function_pointers/prb.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *
+ */
+
+void main()
+{
+	char ar[0];
+	char *p;
+	char str[] = "123";
+
+
+	printf("%ld\n", sizeof(ar));
+	printf("%ld\n", sizeof(p));
+}
