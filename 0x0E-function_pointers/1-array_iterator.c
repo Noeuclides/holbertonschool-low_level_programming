4af05fc8583fe8547247d473303ef91fb8a77b6f
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-13 20:53:05 +0000

    pointer to function that prints an array

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,11 +1,23 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
+ * array_iterator - function that calls pointer function with an array
  *
- * Return: 
+ *@array: array
+ *
+ *@size: size of the array
+ *
+ *@action: function
+ *
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
 
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
