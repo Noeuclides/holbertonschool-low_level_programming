eda64f36c2f3fb30ca63b2455fe56fa437b1e0f7
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-14 02:01:35 +0000

    programm that performs simple operations

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,11 +1,27 @@
 #include "function_pointers.h"
 
 /**
- * op_add -
+ * main - calculate an operation
  *
- * Return: 
+ *@argc: number of arguments
+ *
+ *@argv: array of string
+ *
+ * Return: 0 when succeed
  */
-int op_add(int a, int b)
-{
 
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	calc = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", calc);
+	return (0);
 }
