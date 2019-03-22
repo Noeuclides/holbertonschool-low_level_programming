78525890464570d3f78edd32f70dd5d78100cd80
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-22 00:44:03 +0000

    print before main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *print_before - print before main
+ *
+ */
+
+void print_before(void) __attribute__ ((constructor))
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
