2d0593e8027afefd97e21da1c7f3363b654461a2
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-22 02:51:16 +0000

    free list with string

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * free_list -
+ * free_list - free list
  *
- * Return: 
+ *@head: variable type structure to free
+ *
+ * Return: nothing
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 
 }
