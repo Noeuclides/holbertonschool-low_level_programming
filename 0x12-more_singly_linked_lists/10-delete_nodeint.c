f1f3337b454687e72c121311976ce51008b2a71d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-26 21:43:43 +0000

    delete any given index node

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,11 +1,49 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index -
+ * delete_nodeint_at_index - function that deletes the node at index of a list
  *
- * Return: 
+ *@head: pointer to pointer that points to a list
+ *
+ *@index: node that should deleted
+ *
+ * Return: 1 if succeed, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *nodetodel, *firstnode;
+	unsigned int i = 0;
 
+	firstnode = *head;
+	if (index != 0)
+	{
+		while ((i + 1) < index && (*head)->next != NULL)
+		{
+			*head = (*head)->next;
+			i++;
+		}
+	}
+	else
+	{
+		if (*head != NULL)
+		{
+			*head = (*head)->next;
+			firstnode->next = NULL;
+			free(firstnode);
+			return (1);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	if (*head == NULL)
+		return (-1);
+
+	nodetodel = (*head)->next;
+	(*head)->next = nodetodel->next;
+	nodetodel = NULL;
+	*head = firstnode;
+	free(nodetodel);
+	return (1);
 }
