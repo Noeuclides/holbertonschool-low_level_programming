cee7d4671d0a0bcef0aa35f3b9b4746ef13f0bb0
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-26 22:57:05 +0000

    check if index is more than the number of nodes

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -19,6 +19,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
+	if (i < index && head == NULL)
+		return (NULL);
+
 	if (head == NULL)
 		return (NULL);
 	return (head);
