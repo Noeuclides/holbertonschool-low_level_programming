d834622c3c29e99b7f3f5e9ec47dfc2dd34d70ed
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-26 16:41:11 +0000

    delete first(head) node of a list

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * pop_listint -
+ * pop_listint - function that deletes the head node of a list
  *
- * Return: 
+ *@head: list of struct
+ *
+ * Return: the head node's data
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *aux;
+	int n;
 
+	aux = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+	aux->next = NULL;
+	free(aux);
+
+	return (n);
 }
