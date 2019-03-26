43bb99f8b772f720babffa2bf7a569ead8a1d47e
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-26 15:55:34 +0000

    free list with pointer and with pointer to pointer

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -3,11 +3,18 @@
 /**
  * free_listint - function that free a lost
  *
- *head: node to free
+ *@head: node to free
  *
  * Return: void
  */
 void free_listint(listint_t *head)
 {
+	listint_t *freenode;
 
+	while (head != NULL)
+	{
+		freenode = head;
+		head = head->next;
+		free(freenode);
+	}
 }
--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
+ * free_listint2 - function that frees a list and sets head to NULL
  *
- * Return: 
+ *@head: pointer to pointer that points to a struct
+ *
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *freenode2;
 
+	while (*head != NULL)
+	{
+		freenode2 = *head;
+		*head = (*head)->next;
+		free(freenode2);
+	}
+	*head = NULL;
 }
