29582bae464b752ee4a276141942b2286721bbaf
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-28 23:38:43 +0000

    function that convert unsigned integer to binary with format

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 
 /**
- * print_binary -
+ * print_binary - function that prints the binary representation of a number
  *
- * Return: 
+ *@n: unsigned integer to be converted
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int m;
 
+	if (n > 1)
+	{
+		m = n >> 1;
+		print_binary(m);
+		_putchar((n & 1) + 48);
+	}
+	else
+	{
+		if (n == 0)
+			_putchar('0');
+		else
+			_putchar((n & 1) + 48);
+	}
 }
