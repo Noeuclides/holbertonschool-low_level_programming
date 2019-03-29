d9cf96d09323c1cc30fd619baa9441bcd7f5548e
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-29 01:07:16 +0000

    return the value of a bit at a given index

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
 
 /**
- * get_bit -
+ * get_bit - function that returns the value of a bit
  *
- * Return: 
+ *@n: number in decimal system to evaluate
+ *
+ *@index: bit to be found
+ *
+ * Return: the value of a bit
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int i;
 
+	if (index > (sizeof(n) * 8))
+		return (-1);
+
+	i = n >> index & 1;
+
+	return (i);
 }
