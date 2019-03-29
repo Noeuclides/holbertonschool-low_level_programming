0611a83ced73e9a14e387f377aa259471c21b1c5
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-29 03:21:56 +0000

    number of fliped numbers from one integer to another

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
+ * flip_bits - function that return the number of bits to flip from n to m
  *
- * Return: 
+ *@n: first number to compare
+ *
+ *@m: second number to compare
+ *
+ * Return: the number of bits fliped
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int nb, mb;
 
+	if (n == 0 && m == 0)
+		return (0);
+	if ((n & 1) == (m & 1))
+	{
+		nb = n >> 1;
+		mb = m >> 1;
+		return (0 + flip_bits(nb, mb));
+	}
+
+	nb = n >> 1;
+	mb = m >> 1;
+	return (1 + flip_bits(nb, mb));
 }
