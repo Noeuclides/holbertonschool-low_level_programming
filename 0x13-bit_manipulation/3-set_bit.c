303bd93ff26853e078095779498549e7f209264c
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-29 02:07:59 +0000

    set a bit with 1

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
 
 /**
- * set_bit -
+ * set_bit - function that sets the value of a bit to 1
  *
- * Return: 
+ *@n: pointer to an integer
+ *
+ *@index: index to be changed to 1
+ *
+ * Return: 1 if it worked, -1 if an error ocurred
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int m;
 
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+
+	m = 1 << index;
+	*n = *n | m;
+	return (1);
 }
