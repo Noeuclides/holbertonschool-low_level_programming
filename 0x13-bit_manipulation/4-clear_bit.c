865ab256dc23a56a6b61d1f631fb29318576bd04
Noeuclides <euclidesnoeuclides@gmail.com>
2019-03-29 02:38:04 +0000

    set a bit to zero

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
+ * clear_bit - function that sets the value of a bit to 0
  *
- * Return: 
+ *@n: pointer to a number to evaluate
+ *
+ *@index: index of the number to be changed
+ *
+ * Return: 1 if it worked -1 if an error ocurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > (sizeof(*n) * 8))
+		return (-1);
 
+	*n = *n & ~(1 << index);
+	return (1);
 }
