e348adf26be51cfa120656a40efc4f3b7158160b
Noeuclides <euclidesnoeuclides@gmail.com>
2019-04-03 21:40:01 +0000

    append text to a file with filename == NULL

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -15,7 +15,11 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i, w;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_RDWR | O_APPEND);
+
 	if (fd == -1)
 		return (-1);
 
