9f57984f1a369afc2d08b139d26c370e583940b7
Noeuclides <euclidesnoeuclides@gmail.com>
2019-04-03 21:15:18 +0000

    create a file with text

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,11 +1,34 @@
+#include <unistd.h>
+#include <fcntl.h>
 #include "holberton.h"
 
+
 /**
- * create_file -
+ * create_file - create a file
  *
- * Return: 
+ *@filename: name of the file to create
+ *
+ *@text_content: string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
+
+	int fd, i, w;
+
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	for (i = 0; *(text_content + i) != '\0'; i++)
+		;
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
+	return (1);
 
 }
