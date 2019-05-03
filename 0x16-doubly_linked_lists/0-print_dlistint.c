9d95b6ebddffa2bd4d78c09a9723937f8fecf9f5
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-03 21:52:04 +0000

    double linked list updating project

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of a structure
+ *
+ * Return: 
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+	
+	for(nodes = 0; h != NULL; nodes++)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return(nodes); 
+}	
