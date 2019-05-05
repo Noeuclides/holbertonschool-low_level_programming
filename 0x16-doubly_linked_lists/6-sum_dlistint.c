eee5dfb816ef362b9c8107090092f8dcdcbbda9d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-05 04:53:58 +0000

    sum elements of a list

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
+ * sum_dlistint - function that returns the sum of all the data (n) of a list.
  *
- * Return: 
+ * @head: head element of a list
+ *
+ * Return: 0 if the list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *node;
+	int i, sum = 0;
+
+	if (head == NULL)
+		return (0);
+	node = head;
+	for (i = 0; node != NULL; i++)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+	return (sum);
 
 }
