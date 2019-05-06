c96ad2c7d392cec20d1f63c8058f1ef5e7411c82
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-06 03:27:55 +0000

    insert node

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		ins = ins->next;
 		if (ins == NULL)
-			return(NULL);
+			return (NULL);
 	}
 
 	aux = ins->prev;
