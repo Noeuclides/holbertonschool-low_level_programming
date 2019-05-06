1bf04d3c36ea43bc8317830843023b93f17fa4b7
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-06 00:13:26 +0000

    insert node in a list

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,54 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index -
+ * create_nodeint - function that creates a node of a structure type
  *
- * Return: 
+ * @new: new node to create
+ *
+ * @n: integer data of the structure
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *create_nodeint(dlistint_t *new, int n)
+{
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
+
+/**
+ * insert_dnodeint_at_index - insert a new node at a given position
+ *
+ * @h: head element of a list
+ *
+ * @idx: index of the node to insert
+ *
+ * @n: integer element of the node
+ *
+ * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new = NULL, *ins, *aux;
+	unsigned int i;
 
+	new = create_nodeint(new, n);
+	ins = *h;
+	for (i = 0; i < idx; i++)
+	{
+		ins = ins->next;
+	}
+	aux = ins->prev;
+	ins->prev = new;
+	aux->next = new;
+	new->next = ins;
+	new->prev = aux;
+	return (new);
 }
