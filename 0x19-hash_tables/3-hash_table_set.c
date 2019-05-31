0e1df38f65bf0fac3b527f1afa03eb94df0b33a3
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-31 22:59:36 +0000

    hash tables create

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,30 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set -
+ * hash_table_set - add element to hash table
  *
- * Return: 
+ * @ht: hash table
+ *
+ * @key: key
+ *
+ * @value: value asociated to the key
+ *
+ * Return: 1 if succeed, 0 if fails
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int hash;
-	hash_node_t *new, *head = NULL;
+	char *k_aux = NULL, *v_aux = NULL;
+	hash_node_t *new = NULL;
 
-	new = malloc(sizeof(hash_node_t *));
+	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (0);
 
-	hash = hash_djb2((const unsigned char*)key);
-	ht->array[hash]->key = (char *)key;
-	ht->array[hash]->value = (char *)value;
-	if (head == NULL)
-	{
-		head = ht->array[hash];
-		return (1);
-	}
-	
-	for (new = head; new->next != NULL; new = new->next)
-		;
+	k_aux = strdup(key);
+	v_aux = strdup(value);
+	hash = hash_djb2((const unsigned char *)key);
+	new->key = k_aux;
+	new->value = v_aux;
+	new->next = ht->array[hash];
 	ht->array[hash] = new;
 
 	return (1);
