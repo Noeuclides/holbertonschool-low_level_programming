00d8ed3bc641b449d51383a1d2efa8542fbf29d2
Noeuclides <euclidesnoeuclides@gmail.com>
2019-05-31 19:19:42 +0000

    hash tables set and access tests

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -7,5 +7,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	int hash;
+	hash_node_t *new, *head = NULL;
 
+	new = malloc(sizeof(hash_node_t *));
+	if (!new)
+		return (0);
+
+	hash = hash_djb2((const unsigned char*)key);
+	ht->array[hash]->key = (char *)key;
+	ht->array[hash]->value = (char *)value;
+	if (head == NULL)
+	{
+		head = ht->array[hash];
+		return (1);
+	}
+	
+	for (new = head; new->next != NULL; new = new->next)
+		;
+	ht->array[hash] = new;
+
+	return (1);
 }
--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -7,5 +7,15 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	int hash;
 
+	hash = hash_djb2((const unsigned char*)key);
+
+       	if (strcmp((const char *)ht->array[hash]->value, key) != 0)
+	{
+		printf("different");
+		return (NULL);
+	}
+		
+	return (ht->array[hash]->value);
 }
