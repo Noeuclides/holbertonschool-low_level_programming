b671a7a1990a49570a49c2a4e54acd196cac5f7c
Noeuclides <euclidesnoeuclides@gmail.com>
2019-06-01 02:45:19 +0000

    hash tables print

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,11 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - prints a hash table.
  *
- * Return: 
+ * @ht: hash table
+ *
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *aux;
+	unsigned long int i, j;
 
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		while(aux != NULL)
+	       	{
+       			printf("'%s': '%s'", aux->key, aux->value);
+			j = i;
+			aux = aux->next;
+			if (aux != NULL)
+				printf(", ");
+		}
+		if (i > j && ht->array[i + 1] != NULL)
+			printf(", ");
+
+	}
+	printf("}\n");
 }
