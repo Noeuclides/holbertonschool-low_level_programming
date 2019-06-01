3bba68551e92da26b48d3623fa4b8d30be7d801a
Noeuclides <euclidesnoeuclides@gmail.com>
2019-06-01 03:09:03 +0000

    hash tables delete

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -27,7 +27,7 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 		}
 		if (i > j && ht->array[i + 1] != NULL)
-			printf(", ");
+			printf(", n");
 	}
 	printf("}\n");
 }
--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -3,9 +3,29 @@
 /**
  * hash_table_delete -
  *
+ * @ht: 
+ *
  * Return: 
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *del;
+	unsigned long int i;
 
+	for (i = 0; i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		aux = del;
+		while (del->next != NULL)
+		{
+			del = del->next;
+		}
+		free(del);
+		while (ht->array[i] != NULL)
+		{
+			
+		}
+		
+		
+	}
 }
