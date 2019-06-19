af9c0051daa43d9b5e723bc621f7b31bbbb1e26d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-06-19 14:17:43 +0000

    [feature] shell sort knuth sequence

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -26,5 +26,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 int partition(int *array, int first, int last, size_t size);
 void quick_sort_r(int *array, int first, int last, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
