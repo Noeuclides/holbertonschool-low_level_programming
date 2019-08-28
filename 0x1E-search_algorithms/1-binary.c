8991109e24d843c38148b1bc4f4a31664d4ed39d
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-28 20:29:56 +0000

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,39 +1,42 @@
 #include "search_algos.h"
 
 /**
+ * binary_search - function that makes a binary search to find a number
  *
+ * @array: pointer to position 0 of an array of integers
+ * @size: size of the array
+ * @value: value to find
  *
- *
- *
+ * Return: the index where is the value or -1 if it isn't in the array
  */
 
 int binary_search(int *array, size_t size, int value)
 {
 	int *aux = array;
-	size_t i, j, k = 0;
+	size_t i = 0, k = size - 1, j = 0;
 
 	if (!array)
-		return(-1);
+		return (-1);
 
-	for (i = k; i < size - 1; i++)
+	while (i <= k)
 	{
+		j = i;
 		printf("Searching in array: ");
-		for (j = k; j < size - 1; j++)
+		for (j = i; j < k; j++)
+		{
 			printf("%d, ", *(aux + j));
+		}
 
 		printf("%d\n", *(aux + j));
 
-		aux = array;
-		if (size % 2 == 0)
-			size++;
-		printf("size / 2 = %lu\n", size / 2);
-		if (*(aux + (size / 2) - 1) == value)
-			return(size / 2);
-		else if (*(aux + (size / 2) - 1) < value)
-			k = size / 2;
+		if (*(aux + (i + k) / 2) < value)
+			i = ((i + k) / 2) + 1;
+		else if (*(aux + (i + k) / 2) > value)
+			k = ((i + k) / 2) - 1;
 		else
-			size = (size / 2) - 1 - k;
+			return ((i + k) / 2);
+
 	}
-	return(-1);
+	return (-1);
 
 }
