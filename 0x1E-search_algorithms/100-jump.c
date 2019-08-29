7a4f7419508ddc441ee6995d699cd40a42556ded
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-29 07:31:07 +0000

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 /**
- * binary_search - function that makes a binary search to find a number
+ * jump_search - function that makes a binary search to find a number
  *
  * @array: pointer to position 0 of an array of integers
  * @size: size of the array
@@ -13,32 +13,33 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step = sqrt(size), f = 1, lim_izq = 0, lim_der = 0, i = 0;
+	size_t step = sqrt(size), f = 1, l_izq = 0, l_der = 0, i = 0;
+	int val = 0;
 
 	if (!array)
-		return(-1);
+		return (-1);
 
-	lim_der = step;
-	while(*(array + lim_izq) < value && f == 1)
+	l_der = step;
+	while (*(array + l_izq) < value && f == 1)
 	{
-		printf("Value checked array[%lu] = [%d]\n", lim_izq, *(array + lim_izq));
-		lim_izq = lim_der;
-		lim_der += sqrt(size);
-		if (lim_izq > size)
+		val = *(array + l_izq);
+		printf("Value checked array[%lu] = [%d]\n", l_izq, val);
+		l_izq = l_der;
+		l_der += sqrt(size);
+		if (l_izq > size)
 			f = 0;
 	}
-	lim_der -= step;
-	lim_izq -= step;
-	printf("Value found between indexes [%lu] and [%lu]\n", lim_izq, lim_der);
-	if (lim_der > size - 1)
-		lim_der = size -1;
-	for ( i = lim_izq; i <= lim_der; i++)
+	l_der -= step;
+	l_izq -= step;
+	printf("Value found between indexes [%lu] and [%lu]\n", l_izq, l_der);
+	if (l_der > size - 1)
+		l_der = size - 1;
+	for (i = l_izq; i <= l_der; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, *(array + i));
-		if(value == *(array + i))
-			return(i);
+		if (value == *(array + i))
+			return (i);
 	}
-
-	return(-1);
+	return (-1);
 
 }
