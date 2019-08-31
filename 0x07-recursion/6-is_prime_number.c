9ff20d7712bdb6c4a5fbc6f93b39c196714f11e4
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-31 23:23:43 +0000

    check prime recursive

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,28 +1,43 @@
 #include "holberton.h"
 
 /**
- *is_prime_number - primer numbers recursive
+ *prime - check if it is prime number recursive function
  *
- *@n: integer
+ *@n: number to evaluate if it is prime
  *
- *Return: 1 if n is prime, 0otherwise
+ *@div: number to div n to see if it's multiple
+ *
+ *@count: number to count how many times has been divisible n
+ *
+ *Return: 1 if n is prime 0 otherwise
  */
+int prime(int n, int div, int count)
+{
+	div++;
 
+	if (count >= 2)
+		return (0);
+	else if (n == div)
+		return (1);
+	if (n % div == 0)
+		count++;
+	return (prime(n, div, count));
+}
+
+
+/**
+ *is_prime_number - check if it is prime number recursive function
+ *
+ *@n: number to evaluate if it is prime
+ *
+ *Return: 1 if n is prime 0 otherwise
+ */
 int is_prime_number(int n)
 {
-	int m;
+	int count = 1, div = 1;
 
-	if (n / 2 > 1)
-	{
-		m = n;
-		return (funaux(m, n));
-	}
-	else if (n / 2 == 1 || n / 2 == 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+	if (n == 1 || n == -1)
+		return (0);
+
+	return (prime(n, div, count));
 }
